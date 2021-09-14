804e61d92a162838150076ebfe6716b5b868d222
Soheil Forouzan <83650449+SoheilForouzan@users.noreply.github.com>
2021-09-14 18:07:52 +0430

    Update btc-email.py

--- btc-email.py
+++ btc-email.py
@@ -1,3 +1,4 @@
+# Requirements
 import smtplib
 import requests
 import time
@@ -5,21 +6,22 @@ from email.message import EmailMessage
 
 from requests.api import request
 
-
-
-password = 'migo2003' #input('[?] Enter your email password >>>> ')
-email = "btctestermigo@gmail.com"
+# Fake email Info
+password = 'passwd' # Sender password
+#input('[?] Enter your email password >>>> ')
+email = "Email" # Sender email
 cryptoLink = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 
 while True:
+    # Get Crypto data
     text = float(requests.get(cryptoLink).json()[0]['current_price'])
     print(f'[$] Price is : {text}')
+    # Try to send the email
     try:
-
         msg = EmailMessage()
         msg['Subject'] = 'BTC Price'
         msg['from'] = email
-        msg['to'] = 'michael20030617knepper@gmail.com'
+        msg['to'] = 'email' # receiver email 
         msg.set_content(f'BTC Price : {text}')
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
