da9f3e7f2e2ddfb578efb688c51a244f35175474
SoheilForouzan <soheil.forouzan@protonmail.com>
2021-10-24 17:28:24 +0330

    pkg updates

--- pkg-setup.py
+++ pkg-setup.py
@@ -1,45 +1,47 @@
 import argparse
 import os
 
-# terminal colors
+# Terminal colors
 
-reset_color = '\033[0m'
-green = '\033[32m'
-red = '\033[31m'
-yellow='\033[93m'
+reset_color = "\033[0m"
+green = "\033[32m"
+red = "\033[31m"
+yellow = "\033[93m"
+cyan = "\033[36m"
 
 # Python packages only for linux
 
 python = [
-    'django', 'kivy', 'flask',
-	'pillow', 'flask', 'ipython',
-	'selenium', 'scapy', 'whois',
-	'pandas', 'pytelegrambotapi',
-	'telebot', 'python-dotenv', 'autopep8',
-	'secure-smtplib', 'pyperclip3','random2',
-	'paramiko', 'opencv-python','scipy',
-	'bs4', 'datetime',
+    "django", "kivy", "flask",
+    "pillow", "flask", "ipython",
+    "selenium", "scapy", "whois",
+    "pandas", "pytelegrambotapi",
+    "telebot", "python-dotenv", "autopep8",
+    "secure-smtplib", "pyperclip3", "random2",
+    "paramiko", "opencv-python", "scipy",
+    "bs4", "datetime",
 ]
 
 # Termux section
 termux_setup = [
-	'termux-setup-storage','apt update', 'apt upgrade',
+    "termux-setup-storage", "apt update", "apt upgrade",
 ]
 
 termux_install = [
-	'coreutils', 'openssh', 'git',
-	'tor', 'neofetch','unstable-repo',
-	'x11-repo', 'python', 'python-pip',
-	'zsh'
-]
-termux_py = [
-	'ipython', 'scapy', 'whois',
-	'pytelegrambotapi', 'telebot', 'python-dotenv',
-	'secure-smtplib', 'pyperclip3', 'random2',
-	'datetime', 'bs4', 'paramiko'
+    "coreutils", "openssh", "git",
+    "tor", "neofetch", "unstable-repo",
+    "x11-repo", "python-pip", "zsh"
 ]
 
-# Flags
+termux_py = [
+    "ipython", "scapy", "whois",
+    "pytelegrambotapi", "telebot", "python-dotenv",
+    "secure-smtplib", "pyperclip3", "random2",
+    "datetime", "bs4", "paramiko"
+]
+
+# Script Flags
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-l", "--linux",
@@ -50,93 +52,98 @@ parser.add_argument("-t", "--termux",
 args = parser.parse_args()
 
 
-# Python pkg installation
+# Linux Python pkg installation
 
 def python_pkgs():
-	for pkg in python:
-		print(pkg)
-	print('\n')
-	starting = ''
-	while (starting != 'y' and starting != 'n'):
-		starting = input('[?] Start instalation? (y -> yes, n -> no) ')
-	if starting == "y" or starting == "Y":
-		print(f"{green}[+] started{reset_color}\n")
-		for pkg in python:
-			print(f'{yellow}[*]{pkg}:{reset_color}')
-			os.system(f"pip install {pkg}")
-			print(f'{green}[+] Task Done!{reset_color}\n')
-			
-	elif starting == "n" or starting == "N":
-		print(f"{red}[X] Canceled!{reset_color}")
+    for pkg in python:
+        print(cyan + pkg + reset_color)
+    print("\n")
+    starting = ""
+    while (starting != "y" and starting != "n"):
+        starting = input("[?] Start instalation? (y -> yes, n -> no) ")
+    if starting == "y" or starting == "Y":
+        print(f"{green}[+] started{reset_color}\n")
+        for pkg in python:
+            print(f"{yellow}[*]{pkg}:{reset_color}")
+            os.system(f"pip install {pkg}")
+            print(f"{green}[+] Task Done!{reset_color}\n")
+
+    elif starting == "n" or starting == "N":
+        print(f"{red}[X] Canceled!{reset_color}")
 
 # Termux Settings Setup
 
 def termux_setup():
-	for task in termux_setup:
-		print(task)
-	print("\n")
-	starting = ''
-	while (starting != 'y' and starting != 'n'):
-		starting = input('[?] Start Setup? (y -> yes, n -> no) ')
-	if starting == "y" or starting == "Y":
-		print(f"{green}[+] started{reset_color}\n")
-		for task in termux_setup:
-			print(f'{yellow}[*]{task}:{reset_color}')
-			os.system(task)
-			print(f'{green}[+] Task Done!{reset_color}\n')
-			
-	elif starting == "n" or starting == "N":
-		print(f"{red}[X] Canceled!{reset_color}")
+    for command in termux_setup:
+        print(cyan + command + reset_color)
+    print("\n")
+    starting = ""
+    while (starting != "y" and starting != "n"):
+        starting = input("[?] Start Setup? (y -> yes, n -> no) ")
+    if starting == "y" or starting == "Y":
+        print(f"{green}[+] started{reset_color}\n")
+        for command in termux_setup:
+            print(f"{yellow}[*]{command}:{reset_color}")
+            os.system(command)
+            print(f"{green}[+] Task Done!{reset_color}\n")
+
+    elif starting == "n" or starting == "N":
+        print(f"{red}[X] Canceled!{reset_color}")
 
 # Termux requirements installation
 
 def termux_requirements():
-	for task in termux_install:
-		print(task)
-	print("\n")
-	starting = ''
-	while (starting != 'y' and starting != 'n'):
-		starting = input('[?] Start instalation? (y -> yes, n -> no) ')
-	if starting == "y" or starting == "Y":
-		print(f"{green}[+] started{reset_color}\n")
-		for task in termux_install:
-			print(f'{yellow}[*]{task}:{reset_color}')
-			os.system(f'apt install {task}')
-			print(f'{green}[+] Task Done!{reset_color}\n')
-			
-	elif starting == "n" or starting == "N":
-		print(f"{red}[X] Canceled!{reset_color}")
+    for requirement in termux_install:
+        print(requirement)
+    print("\n")
+    starting = ""
+    while (starting != "y" and starting != "n"):
+        starting = input("[?] Start instalation? (y -> yes, n -> no) ")
+    if starting == "y" or starting == "Y":
+        print(f"{green}[+] started{reset_color}\n")
+        for requirement in termux_install:
+            print(f"{yellow}[*]{requirement}:{reset_color}")
+            os.system(f"apt install {requirement}")
+            print(f"{green}[+] Task Done!{reset_color}\n")
+
+    elif starting == "n" or starting == "N":
+        print(f"{red}[X] Canceled!{reset_color}")
 
 # Termux python dependencies
 
 def termux_python():
-	for pkg in termux_py:
-		print(pkg)
-	print("\n")
-	starting = ''
-	while (starting != 'y' and starting != 'n'):
-		starting = input('[?] Start instalation? (y -> yes, n -> no) ')
-	if starting == "y" or starting == "Y":
-		print(f"{green}[+] started{reset_color}\n")
-		for pkg in termux_install:
-			print(f'{yellow}[*]{pkg}:{reset_color}')
-			os.system(f'pip install {pkg}')
-			print(f'{green}[+] Task Done!{reset_color}\n')
-			
-	elif starting == "n" or starting == "N":
-		print(f"{red}[X] Canceled!{reset_color}")
+    for pkg in termux_py:
+        print(pkg)
+    print("\n")
+    starting = ""
+    while (starting != "y" and starting != "n"):
+        starting = input("[?] Start instalation? (y -> yes, n -> no) ")
+    if starting == "y" or starting == "Y":
+        print(f"{green}[+] started{reset_color}\n")
+        for pkg in termux_install:
+            print(f"{yellow}[*]{pkg}:{reset_color}")
+            os.system(f"pip install {pkg}")
+            print(f"{green}[+] Task Done!{reset_color}\n")
+
+    elif starting == "n" or starting == "N":
+        print(f"{red}[X] Canceled!{reset_color}")
 
 
 def main():
-	if args.linux == "py":
-		python_pkgs()
-	if args.termux == "setup":
-		termux_setup()
-	elif args.termux == "requirements":
-		termux_requirements()
-	elif args.termux == "py":
-		termux_python()
+	# Linux
+
+    if args.linux == "py":
+        python_pkgs()
+
+	# Termux
+
+    if args.termux == "setup":
+        termux_setup()
+    elif args.termux == "requirements":
+        termux_requirements()
+    elif args.termux == "py":
+        termux_python()
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
